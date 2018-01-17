30c4fea615a68bf9941b8a5b1e8f3d0d2e3d77a3
shukladiwakar <csediwakar@gmail.com>
2018-01-18 00:03:56 +0530

    new

---
+++ temp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 07:46:05 2018
+
+@author: diwakar
+"""
+
+import time;
+
+localtime = time.localtime(time.time())
+print ("Local current time :", localtime)
