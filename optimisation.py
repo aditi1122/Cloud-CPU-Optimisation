dd76c38c0c78dde506a4a120508db317f949eed9
shukladiwakar <csediwakar@gmail.com>
2018-01-26 00:13:31 +0530

    dowa

--- optimisation.py
+++ optimisation.py
@@ -62,3 +62,78 @@ plt.title('Comparision between the Test and Predictedd Values')
 plt.xlabel('Total Percentage used')
 plt.ylabel('Cores Used')
 plt.show()
+
+
+
+
+
+
+
+
+
+
+#Linear Regression Check
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Split the data into training/testing sets
+diabetes_X_train = X_train
+diabetes_X_test = X_test
+
+# Split the targets into training/testing sets
+diabetes_y_train = y_train
+diabetes_y_test = y_test
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+# Make predictions using the testing set
+diabetes_y_pred = regr.predict(diabetes_X_test)
+
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The mean squared error
+print("Mean squared error: %.2f"
+      % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
+
+# Plot outputs
+plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
+
+
+
+
+
+#Elastic Nets
+
+from sklearn.linear_model import ElasticNet
+from sklearn.datasets import make_regression
+
+X, y = make_regression(n_features=2, random_state=0)
+regr = ElasticNet(random_state=0)
+regr.fit(X_train, y_train)
+
+
+
+print(regr.coef_) 
+
+print(regr.intercept_) 
+
+y_pred_elas = regr.predict(X_test)
+
+# The mean squared error
+print("Mean squared error: %.2f"
+      % mean_squared_error(diabetes_y_test, y_pred_elas))
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
